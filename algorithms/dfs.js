fc5943002294517b56771e1850c2f85a6b2f840a
Anthony <nixegend@gmail.com>
2016-05-06 13:23:02 +0300

    add dfs algorithm

---
+++ algorithms/dfs.js
@@ -0,0 +1,37 @@
+// Depth-first search
+
+function dfs(start, nodes, callback) {
+    var visitedNodes = [];
+
+    (function goDeep(node, visited) {
+        var childNodes = nodes[node];
+        visited.push(node);
+        visitedNodes = visited;
+
+        for (var n in childNodes) {
+            var node = childNodes[n];
+            if (0 > visited.indexOf(node))
+                goDeep(node, visited);
+        }
+
+    })(start, visitedNodes);
+
+    callback(visitedNodes);
+};
+
+var nodes = {
+    "1": [2, 5, 9],
+    "2": [3],
+    "3": [4],
+    "4": [],
+    "5": [6, 8],
+    "6": [7],
+    "7": [],
+    "8": [],
+    "9": [10],
+    "10": []
+};
+
+dfs(1, nodes, function (arr) {
+    console.log(arr);
+});
